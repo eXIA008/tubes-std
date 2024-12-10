96db980e9c3169f67a36b997bea5cd7a75300a3d
eXIA008 <165605674+eXIA008@users.noreply.github.com>
2024-12-10 19:09:03 +0700

    Upload Base file

---
+++ blogger.h
@@ -0,0 +1,67 @@
+#ifndef blogger_H
+#define blogger_H
+#include <iostream>
+#define namaBlogger(B) B->namaBlogger
+#define emailBlogger(B) B->emailBlogger
+#define kodeBlogger(B) B->kodeBlogger
+#define nextBlogger(B) B->nextBlogger
+#define prevBlogger(B) B->prevBlogger
+#define namaPlatform(P) P->namaPlatform
+#define urlPlatform(P) P->urlPlatform
+#define nextPlatform(P) P->nextPlatform
+#define bloggerID(W) W->bloggerID
+#define platformUsed(W) W->platformUsed
+#define nextWriting(W) W->nextWriting
+#define firstBlogger(B) B.firstBlogger
+#define lastBlogger(B) B.lastBlogger
+#define firstPlatform(P) P.firstPlatform
+#define firstWriting(W) W.firstWriting
+
+typedef blogger *adrBlogger;
+typedef platform *adrPlatform;
+typedef writing *adrWriting;
+
+struct blogger {
+    string namaBlogger, emailBlogger;
+    int kodePenulis;
+    adrBlogger nextBlogger, prevBlogger;
+};
+
+struct platform {
+    string namaPlatform, urlPlatform;
+    adrPlatform nextPlatform;
+};
+
+struct writing {
+    adrBlogger bloggerID;
+    adrPlatform platformUsed;
+    adrWriting nextWriting;
+};
+
+struct List {
+    adrBlogger firstBlogger;
+    adrBlogger lastBlogger;
+    adrPlatform firstPlatform;
+    adrWriting firstWriting;
+};
+
+
+void createListBlogger(List &B);
+adrBlogger createElmBlogger(string nama, string email, int kode);
+void addBlogger(List &B, adrBlogger p);
+void createListPlatform(List &P);
+adrPlatform createElmPlatform(string nama, string url);
+void addPlatform(List &P, adrPlatform p);
+void createListWriting(List &W);
+adrWriting createElmWriting();
+void addWriting (List &B, adrBlogger b, adrPlatform p);
+void print(List B, List P, List W);
+adrBlogger findBlogger(List B, int IDblogger);
+void findPlatform(List B, List P, int IDblogger);
+void countPlatform(List B, List P, int IDblogger);
+void deleteBlogger(List B, List W, adrBlogger &p);
+void deletePlatform(List P, List W, 
+adrPlatform &p, int IDblogger);
+void menu();
+
+#endif
